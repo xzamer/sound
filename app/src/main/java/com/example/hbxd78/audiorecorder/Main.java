6d11a505fa2c7da869ee9889ef9fd16555835e4b
paykey <eilonik@gmail.com>
2016-04-30 20:59:42 +0300

    added dir

--- app/src/main/java/com/example/hbxd78/audiorecorder/Main.java
+++ app/src/main/java/com/example/hbxd78/audiorecorder/Main.java
@@ -1,143 +1,120 @@
 package com.example.hbxd78.audiorecorder;
 
-
-import android.content.Intent;
+import android.app.Activity;
 import android.media.MediaPlayer;
-import android.net.Uri;
+import android.media.MediaRecorder;
+
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+
+import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.example.hbxd78.audiorecorder.dummy.newFrag;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.io.IOException;
 
-public class Main extends FragmentActivity {
-    public static Button play, stop, record, fragment;
+
+public class Main extends Activity {
+    Button play,stop,record;
+    private MediaRecorder myAudioRecorder;
     private String outputFile = null;
-    private MediaPlayer m;
-    private boolean changed = false;
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-
-    private GoogleApiClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        play = (Button) findViewById(R.id.button3);
-        stop = (Button) findViewById(R.id.button2);
-        record = (Button) findViewById(R.id.button);
-        fragment = (Button) findViewById(R.id.button4);
+        play=(Button)findViewById(R.id.button3);
+        stop=(Button)findViewById(R.id.button2);
+        record=(Button)findViewById(R.id.button);
 
-        fragment.setEnabled(true);
         stop.setEnabled(false);
         play.setEnabled(false);
-        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+	File mydir = new File(Environment.getExternalStorageDirectory() + "/soundDir/");
+        if(!mydir.exists()){
+            mydir.mkdirs();
+        }
+        else {
+            filecount = mydir.listFiles().length;
+        }
+        mydir = null;
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/soundDir/recording" + filecount + ".3gp";
+
+        myAudioRecorder=new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
 
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                }
+
+                catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
                 record.setEnabled(false);
                 stop.setEnabled(true);
-                play.setEnabled(false);
-                Intent intent = new Intent(getApplicationContext(),IntentServiceRecord.class);
-                startService(intent);
+
+                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
             }
         });
 
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Audio recorded successfully", Toast.LENGTH_SHORT).show();
+                myAudioRecorder.stop();
+                myAudioRecorder.release();
+                myAudioRecorder  = null;
+
                 stop.setEnabled(false);
                 play.setEnabled(true);
-                record.setEnabled(true);
-                if (m != null) { // Stop playing an existing record.
-                    m.stop();
-                    m.release();
-                    m = null;
-                }
-                stopService(new Intent(getApplicationContext(),IntentServiceRecord.class));
+
+                Toast.makeText(getApplicationContext(), "Audio recorded successfully",Toast.LENGTH_LONG).show();
             }
         });
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) throws IllegalArgumentException, SecurityException, IllegalStateException {
-                Intent intent = new Intent();
-                intent.setType("audio/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,"Select Audio "), 1);
-                play.setEnabled(false);
-                record.setEnabled(false);
-                stop.setEnabled(true);
-                m = new MediaPlayer();
+            public void onClick(View v) throws IllegalArgumentException,SecurityException,IllegalStateException {
+                MediaPlayer m = new MediaPlayer();
+
                 try {
                     m.setDataSource(outputFile);
                 }
+
                 catch (IOException e) {
                     e.printStackTrace();
                 }
+
                 try {
                     m.prepare();
                 }
+
                 catch (IOException e) {
                     e.printStackTrace();
                 }
 
-                /**
-                 * Listen to actively playing media and perform actions on completion.
-                 */
-
-                m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        Toast.makeText(getApplicationContext(), "Finished Playing Audio", Toast.LENGTH_SHORT).show();
-                        record.setEnabled(true);
-                        play.setEnabled(true);
-                        stop.setEnabled(false);
-                    }
-                });
                 m.start();
-                Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
             }
         });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-
-        fragment.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (changed) {
-                   Toast.makeText(getApplicationContext(), "I haven't yet taken care of it, don't spam my app.", Toast.LENGTH_SHORT).show();
-                   return;
-                }
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-// Replace whatever is in the fragment_container view with this fragment,
-// and add the transaction to the back stack so the user can navigate back
-                transaction.replace(R.id.fragment_blank, new newFrag());
-                transaction.addToBackStack(null);
-
-// Commit the transaction
-                transaction.commit();
-                changed = true;
-            }
-        });
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -160,45 +137,5 @@ public class Main extends FragmentActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.hbxd78.audiorecorder/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.hbxd78.audiorecorder/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
